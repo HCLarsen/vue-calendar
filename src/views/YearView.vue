659102ec68eb12545312381eeaf24e7b1e3cf701
Chris Larsen <clarsendev@protonmail.com>
2019-10-14 02:25:26 -0400

    Add days to YearView

--- src/views/YearView.vue
+++ src/views/YearView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="year">
     <h1 v-html="year"></h1>
+    <div class="year">
+      <ul class="months">
+        <li v-for="month in months" :key="month" class="month">
+          <h3 v-html="monthName(month)"></h3>
+          <ul class="days">
+            <li v-for="day in daysInMonth(month)" :key="day" class="day" v-html="day"></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -12,7 +22,59 @@
         type: Number
       }
     },
+    computed: {
+      months() {
+        return Array(12).keys();
+      },
+    },
+    methods: {
+      monthName(month) {
+        return new Date(0, month).toLocaleDateString("en-CA", { month: "long" });
+      },
+      daysInMonth(month) {
+        return new Date(this.year, month + 1, 0).getDate();
+      },
+    }
   }
 </script>
 
-<style scoped></style>
+<style scoped>
+  ul.months {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+
+    list-style-type: none;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  li.month {
+    width: calc(93% / 3);
+    padding: 0 1%;
+  }
+
+  @media screen and (min-aspect-ratio: 1/1) {
+    li.month {
+      width: calc(92% / 4);
+    }
+  }
+
+  ul.days {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+
+    list-style-type: none;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  li.day {
+    width: calc(100% / 7 - 1px);
+  }
+</style>
--- tests/unit/YearView.spec.js
+++ tests/unit/YearView.spec.js
@@ -11,4 +11,17 @@ describe('YearView', () => {
   it('Renders proper header', () => {
     expect(wrapper.text()).toContain('2019');
   });
+
+  it('Renders twelve months', () => {
+    const months = wrapper.findAll('li.month');
+    expect(months.length).toBe(12);
+  });
+
+  it('Renders January', () => {
+    const january = wrapper.findAll('li.month').at(0);
+    expect(january.text()).toContain('January');
+
+    const days = january.findAll('li.day');
+    expect(days.length).toBe(31);
+  });
 });
