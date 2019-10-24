96b7c2bd785abd8c4b24008fc73c86655e5bd7b2
Chris Larsen <clarsendev@protonmail.com>
2019-10-24 13:03:34 -0400

    Improve DayView specs

--- tests/unit/DayView.spec.js
+++ tests/unit/DayView.spec.js
@@ -2,24 +2,30 @@ import { mount } from '@vue/test-utils';
 import DayView from '@/views/DayView'
 
 describe('DayView', () => {
-  const today = new Date();
-
   const wrapper = mount(DayView, {
     propsData: {
-      day: today.getDate(),
-      month: today.getMonth(),
-      year: today.getFullYear()
+      day: 31,
+      month: 9,
+      year: 2019
     }
   });
 
-  it('Renders the correct date', () => {
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const today = new Date();
-    const dateString = today.toLocaleDateString('en-CA', options);
-
+  it('Renders October 31, 2019', () => {
     const header = wrapper.find('h1#date');
-    expect(header.text()).toContain(dateString);
-  })
+    expect(header.text()).toContain('Thursday, October 31, 2019');
+  });
+
+  it('Renders November 5, 1955', () => {
+    const fluxCapacitorDay = mount(DayView, {
+      propsData: {
+        day: 5,
+        month: 10,
+        year: 1955
+      }
+    });
+
+    expect(fluxCapacitorDay.text()).toContain('Saturday, November 5, 1955');
+  });
 
   it('Renders 24 hours of a day', () => {
     const hours = wrapper.findAll('li.hour');
