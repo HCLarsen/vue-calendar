df6a543cad50ddcbfa593c31b3ed7a565e72b601
Chris Larsen <clarsendev@protonmail.com>
2019-10-24 12:53:23 -0400

    Expand CalendarView specs

--- tests/unit/CalendarView.spec.js
+++ tests/unit/CalendarView.spec.js
@@ -2,13 +2,34 @@ import { mount, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 
 import CalendarView from '@/views/CalendarView';
+import YearView from '@/views/YearView';
 import MonthView from '@/views/MonthView';
+import DayView from '@/views/DayView';
 import router from '@/router.js';
 
 const localVue = createLocalVue()
 localVue.use(VueRouter)
 
 describe('CalendarView', () => {
+  it('Renders default path as current month', () => {
+    const wrapper = mount(CalendarView, {
+      localVue,
+      router,
+    });
+
+    expect(wrapper.find(MonthView).exists()).toBe(true);
+  });
+
+  it('Renders 2019', () => {
+    const wrapper = mount(CalendarView, {
+      localVue,
+      router,
+    });
+    router.push("/2019");
+
+    expect(wrapper.find(YearView).exists()).toBe(true);
+  });
+
   it('Renders October 2019', () => {
     const wrapper = mount(CalendarView, {
       localVue,
@@ -19,5 +40,17 @@ describe('CalendarView', () => {
 
     expect(wrapper.find(MonthView).exists()).toBe(true);
     expect(wrapper.text()).toContain('October');
+  });
+
+  it('Renders October 31, 2019', () => {
+    const wrapper = mount(CalendarView, {
+      localVue,
+      router,
+    });
+
+    router.push("/2019/10/31");
+
+    expect(wrapper.find(DayView).exists()).toBe(true);
+    expect(wrapper.text()).toContain('Thursday, October 31');
   });
 });
